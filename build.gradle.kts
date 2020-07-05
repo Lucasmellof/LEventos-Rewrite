4b1716181534e33c2f3f3b9f3245fbd9067f990d
Lucas <lucasmellocem12@gmail.com>
2020-07-05 15:38:24 -0300

    🎉 init: Initializing plugin

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
+    id("net.minecrell.plugin-yml.bukkit") version "0.3.0"
+}
+
+group = "cf.lucasmellof"
+version = "2.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api(fileTree(mapOf("dir" to "lib", "include" to listOf("*.jar"))))
+    api(kotlin("stdlib", KotlinCompilerVersion.VERSION))
+    testImplementation(group = "junit", name = "junit", version = "4.12")
+}
+bukkit {
+    main = "cf.lucasmellof.eventos.LEventos"
+    author = "Lucasmellof"
+    name = "LEventos"
+    softDepend = listOf("Vault")
+}
