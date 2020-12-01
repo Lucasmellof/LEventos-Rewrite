43b6031ff9acdb53d7946f52dc8b7524bafc0d51
Lucas <lucasmellocem12@gmail.com>
2020-12-01 20:12:08 -0300

    Updating main class

--- src/main/kotlin/cf/lucasmellof/eventos/LEventos.kt
+++ src/main/kotlin/cf/lucasmellof/eventos/LEventos.kt
@@ -1,13 +1,14 @@
 package cf.lucasmellof.eventos
 
 import cf.lucasmellof.eventos.commands.EventoCommand
-import cf.lucasmellof.eventos.commands.EventosReloadCommand
 import cf.lucasmellof.eventos.components.EventComponents
 import cf.lucasmellof.eventos.config.ConfigManager
+import cf.lucasmellof.eventos.listeners.PlayerListeners
 import cf.lucasmellof.eventos.types.FastClickEvent
 import cf.lucasmellof.eventos.types.HoverEvent
 import cf.lucasmellof.eventos.types.MathEvent
 import cf.lucasmellof.eventos.utils.Vault
+import me.saiintbrisson.bukkit.command.BukkitFrame
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
@@ -21,17 +22,36 @@ class LEventos : JavaPlugin() {
     }
 
     var runningEvent: EventComponents? = null
+    lateinit var frame: BukkitFrame
+
+    val loadedEvents = hashMapOf<String, EventComponents>()
+
     override fun onEnable() {
         INSTANCE = this
         Bukkit.getConsoleSender().sendMessage("§a ! §fCarregando o plugin de eventos")
         Bukkit.getConsoleSender().sendMessage("§a ! §fPlugin criado por Lucasmellof")
         Bukkit.getConsoleSender().sendMessage("§a ! §fhttps://github.com/Lucasmellof")
         Bukkit.getConsoleSender().sendMessage("§a ! §fDiscord: carmello#0760")
+        registerAndLoadCommands(this)
+        registerEvent(FastClickEvent())
+        registerEvent(MathEvent())
+        registerEvent(HoverEvent())
+        Bukkit.getPluginManager().registerEvents(PlayerListeners(), this)
         ConfigManager.initialize(this)
         Vault.setupEconomy()
-        getCommand("ev").executor = EventoCommand()
-        getCommand("eventosreload").executor = EventosReloadCommand()
-        eventScheduler()
+        //getCommand("ev").executor = EventoCommandOLD()
+        //getCommand("eventosreload").executor = EventosReloadCommand()
+        //eventScheduler()
+
+    }
+
+    private fun registerAndLoadCommands(plugin: JavaPlugin) {
+        frame = BukkitFrame(plugin)
+        frame.registerCommands(EventoCommand())
+    }
+
+    private fun registerEvent(event: EventComponents) {
+        loadedEvents[event.name] = event
     }
 
     private fun eventScheduler() {
