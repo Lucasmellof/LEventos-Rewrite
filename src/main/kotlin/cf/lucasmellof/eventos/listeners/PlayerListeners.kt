8a91ac562ac441edc903664a9c5bac9836daa82e
Lucas <lucasmellocem12@gmail.com>
2020-12-01 20:09:44 -0300

    Now user don't need to use command to win an event

---
+++ src/main/kotlin/cf/lucasmellof/eventos/listeners/PlayerListeners.kt
@@ -0,0 +1,23 @@
+package cf.lucasmellof.eventos.listeners
+
+import cf.lucasmellof.eventos.LEventos
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.AsyncPlayerChatEvent
+
+/* 
+ * @author Lucasmellof, Lucas de Mello Freitas created on 01/12/2020
+ */
+class PlayerListeners : Listener {
+    @EventHandler
+    fun onMessage(event: AsyncPlayerChatEvent) {
+        if (LEventos.INSTANCE.runningEvent == null) {
+            return
+        }
+        val evento = LEventos.INSTANCE.runningEvent!!
+        if (evento.onPlayer(event.player, event.message)) {
+            evento.onFinish(event.player)
+            event.isCancelled = true
+        }
+    }
+}
