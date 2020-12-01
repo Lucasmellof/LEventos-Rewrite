a82bd0e93b22b79c2f0df506e97fba725b5ac751
Lucas <lucasmellocem12@gmail.com>
2020-12-01 20:57:38 -0300

    Updating ConfigManager

--- src/main/kotlin/cf/lucasmellof/eventos/config/ConfigManager.kt
+++ src/main/kotlin/cf/lucasmellof/eventos/config/ConfigManager.kt
@@ -10,13 +10,29 @@ object ConfigManager {
     lateinit var config: ConfigAPI
 
     var prize: Double = 0.0
+    var prizeCommand: String = ""
+    var prizeEnabled = false
+
+    var autoStartEnabled = false
     var autoStartTime: Long = 0
+
+    var disabledEvents = listOf<String>()
 
     fun initialize(instance: Plugin) {
         config = ConfigAPI(instance, "config.yml", true, header = true)
 
-        prize = config.getDouble("prize")
-        autoStartTime = config.getLong("autostart.time")
+        config.theFile.writer().appendln("CASO NÃO QUEIRA HABILITAR O DINHEIRO DEIXE COMO 0.0")
+        prize = config.getOrSetDefaultValue("prize.money", 0.0)
+        config.theFile.writer().appendln("CASO NÃO QUEIRA HABILITAR O COMANDO DEIXE COMO ''")
+        prizeCommand = config.getOrSetDefaultValue("prize.command", "")
+        prizeEnabled = config.getOrSetDefaultValue("prize.enabled", false)
+        config.theFile.writer()
+            .appendln("PARA PEGAR O NOME DOS EVENTOS USE /EVENTOS LIST E COLOQUE EM LETRAS MINÚSCULAS")
+
+        disabledEvents = config.getOrSetDefaultValue("disabledEvents", listOf())
+
+        autoStartTime = config.getOrSetDefaultValue("autostart.time", 12000L)
+        autoStartEnabled = config.getOrSetDefaultValue("autostart.enabled", false)
 
         config.save()
     }
