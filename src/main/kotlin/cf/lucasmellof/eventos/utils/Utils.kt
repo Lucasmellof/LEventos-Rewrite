36fdc0f457fe12d7958f7d7681717e3b7b2511e8
Lucas <lucasmellocem12@gmail.com>
2020-12-23 00:53:20 -0300

    Adding way to show fancy time without bugs

--- src/main/kotlin/cf/lucasmellof/eventos/utils/Utils.kt
+++ src/main/kotlin/cf/lucasmellof/eventos/utils/Utils.kt
@@ -1,5 +1,6 @@
 package cf.lucasmellof.eventos.utils
 
+import org.apache.commons.lang3.time.DurationFormatUtils
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
 import org.bukkit.plugin.Plugin
@@ -16,5 +17,22 @@ object Utils {
                 Bukkit.dispatchCommand(sender, commands)
             }
         }.runTask(plugin)
+    }
+
+    fun getFancyTime(value: Long): String {
+        var result: String = DurationFormatUtils.formatDuration(value, "d").toString() + "d"
+        if ("0d" == result) {
+            result = DurationFormatUtils.formatDuration(value, "H").toString() + "h"
+            if ("0h" == result) {
+                result = DurationFormatUtils.formatDuration(value, "m").toString() + "m"
+                if ("0m" == result) {
+                    result = DurationFormatUtils.formatDuration(value, "s").toString() + "s"
+                    if ("0s" == result) {
+                        result = value.toString() + "ms"
+                    }
+                }
+            }
+        }
+        return result
     }
 }
